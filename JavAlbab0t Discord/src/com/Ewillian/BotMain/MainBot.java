735ccb2c2b9d83cf81fe2fdd8904e558e792533c
Ewillian <guillaume.lecoq@ynov.com>
2019-04-04 18:02:39 +0200

    Added playing activity

--- JavAlbab0t Discord/src/com/Ewillian/BotMain/MainBot.java
+++ JavAlbab0t Discord/src/com/Ewillian/BotMain/MainBot.java
@@ -1,10 +1,13 @@
+package com.Ewillian.BotMain;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 
-public class BotMain {
+public class MainBot {
 
 	public static JDA jda;
 	
@@ -13,7 +16,8 @@ public class BotMain {
 
 		JDA jda = new JDABuilder(AccountType.BOT)
 				.setToken("").build();
-		
+		jda.getPresence().setStatus(OnlineStatus.IDLE);
+		jda.getPresence().setActivity(Activity.playing("Warframe"));
 	}
 
 }
---
+++ JavAlbab0t Discord/src/com/Ewillian/Commands/Commands.java
@@ -0,0 +1,5 @@
+package com.Ewillian.Commands;
+
+public class Commands {
+
+}
