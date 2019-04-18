a64ac46afe354702f1d430c233a66b165d231ba4
Ewillian <guillaume.lecoq@ynov.com>
2019-04-18 17:25:13 +0200

    Added info

--- JavAlbab0t Discord/src/com/Ewillian/BotMain/MainBot.java
+++ JavAlbab0t Discord/src/com/Ewillian/BotMain/MainBot.java
@@ -1,6 +1,8 @@
 package com.Ewillian.BotMain;
 import javax.security.auth.login.LoginException;
 
+import com.Ewillian.Commands.Commands;
+
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -11,13 +13,14 @@ public class MainBot {
 
 	public static JDA jda;
 	
-	
 	public static void main(String[] args) throws LoginException, InterruptedException {
-
+		System.out.println("Alive");
 		JDA jda = new JDABuilder(AccountType.BOT)
 				.setToken("").build();
-		jda.getPresence().setStatus(OnlineStatus.IDLE);
+		jda.getPresence().setStatus(OnlineStatus.ONLINE);
 		jda.getPresence().setActivity(Activity.playing("Warframe"));
+		
+		jda.addEventListener(new Commands());
 	}
 
 }
--- JavAlbab0t Discord/src/com/Ewillian/Commands/Commands.java
+++ JavAlbab0t Discord/src/com/Ewillian/Commands/Commands.java
@@ -1,5 +1,59 @@
 package com.Ewillian.Commands;
 
-public class Commands {
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.util.ArrayList;
 
+public class Commands extends ListenerAdapter {
+	ArrayList<String> myCommands = new ArrayList<String>();
+	
+	public void onMessageReceived(MessageReceivedEvent  event) {
+		
+        User author = event.getAuthor();
+        Message message = event.getMessage();
+        MessageChannel channel = event.getChannel();
+        String msg = message.getContentDisplay();
+        ArrayList<String> myCommands = new ArrayList<String>();
+        
+        System.out.println("Message received !");
+        System.out.println(msg);
+        
+        if (event.isFromType(ChannelType.TEXT)) {
+        	
+        	if (msg.equals("<!ping"))
+            {
+                channel.sendMessage("pong!").queue();
+            }else if (msg.equalsIgnoreCase("<!help"))
+            {
+        		String aswer = "Liste des commandes : \n ";
+                myCommands.add("<!help - List commands list");
+                myCommands.add("<!ping - Answer pong!");
+                for (String com : myCommands) {
+                	aswer += "`"+com+"` \n";
+				}
+                channel.sendMessage(aswer).queue();
+            }else if (msg.equalsIgnoreCase("<!info"))
+            {
+            	
+            	EmbedBuilder info = new EmbedBuilder();
+            	info.setTitle("✨InesB0t✨");
+            	info.setDescription("Bonjour, je suis l'interface numérique d'échange sociaux ou Ines.");
+            	info.setColor(0x42ebf4);
+            	info.setImage(event.getGuild());
+            	info.setFooter("Created by Ewillian", event.getGuild().getOwner().getUser().getAvatarUrl());
+            	channel.sendMessage(info.build()).queue();
+            }
+        }else if (event.isFromType(ChannelType.PRIVATE)) {
+        	if (msg.equalsIgnoreCase("Salut"))
+            {
+        		channel.sendMessage("Yo!").queue();
+            }
+        }
+	}
 }
